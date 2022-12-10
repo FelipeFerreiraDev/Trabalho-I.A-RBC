6fb7c508c19fd22f4e802d9c4a31777f95dbe798
joão lorenzo malutta haidar <lorenzo.malutta@gmail.com>
2022-12-10 13:17:15 -0300

    :wrench: feat router instalado

--- Front-end/similaridade-ia/src/main.tsx
+++ Front-end/similaridade-ia/src/main.tsx
@@ -1,9 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {App} from './App'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import { Home } from './view/home';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+      element: <Home/>,
+  },
+  {
+    path: "/",
+      element: <Home/>,
+  },
+  {
+    path: "/",
+      element: <Home/>,
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
