8aaed5d9682bb174c141bba4505e4941f963291f
GeovaneRigonato <geovane.rigonato.gil@gmail.com>
2023-01-09 15:58:12 -0300

    :lipstick: feat footer

--- Front-end/similaridade-ia/src/components/footer/index.tsx
+++ Front-end/similaridade-ia/src/components/footer/index.tsx
@@ -1,8 +1,8 @@
 
 export function Footer() {
   return (
-    <footer className="flex justify-center bg-[#000000ca] h-16">
-      <h3 className="text-white">Glauco Lindo</h3>
+    <footer className="flex justify-center bg-[#000000ca] h-16"> 
+      <h3 className="text-white">【 ͡❛ ͜ʖ ͡❛】 Glauco Lindo e GOSTOSO 【 ͡❛ ͜ʖ ͡❛】</h3>
     </footer>
   )
 }
