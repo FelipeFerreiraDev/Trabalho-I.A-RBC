5afb69b45ba3597085abbf0886ae869afa22d3f7
joão lorenzo malutta haidar <lorenzo.malutta@gmail.com>
2023-01-09 14:00:59 -0300

    :lipstick: feat add opções no formulário

--- Front-end/similaridade-ia/src/view/casos/index.tsx
+++ Front-end/similaridade-ia/src/view/casos/index.tsx
@@ -3,50 +3,291 @@ import { useState } from "react"
 export function Casos() {
   const [casosProblema, setCasosProblema] = useState<[]>([])
   return (
-    <section className="h-screen">
-      <div>
-        <h1>CBR | Preencha com o caso problema</h1>
-        <form className="grid grid-cols-2 gap-x-56 gap-y-5 h-full shadow-2xl bg-[#f5f5f5] p-5 rounded-md">
-          <div className="">
-            <label>Área danificada</label>
-            <select className="">
-              <option value="">Selecione o caso problema</option>
-              {casosProblema.map((casoProblema) => (
-                <option value={casoProblema}>{casoProblema}</option>
-              ))}
-            </select>
-          </div>
-          <div>
-            <label>Área danificada</label>
-            <select className="">
-              <option value="">Selecione o caso problema</option>
-              {casosProblema.map((casoProblema) => (
-                <option value={casoProblema}>{casoProblema}</option>
-              ))}
-            </select>
-          </div>
-          <div>
-            <label>Área danificada</label>
-            <select className="">
-              <option value="">Selecione o caso problema</option>
-              {casosProblema.map((casoProblema) => (
-                <option value={casoProblema}>{casoProblema}</option>
-              ))}
-            </select>
-          </div>
-          <div>
-            <label>Área danificada</label>
-            <select className="">
-              <option value="">Selecione o caso problema</option>
-              {casosProblema.map((casoProblema) => (
-                <option value={casoProblema}>{casoProblema}</option>
-              ))}
-            </select>
-          </div>
-        </form>
-        <div className="flex w-24 mt-4">
-          <button type="submit" className="bg-gray-300 duration-500">Enviar</button>
+    <section>
+      <h1>CBR | Preencha com o caso problema</h1>
+      <form className="grid grid-cols-2 gap-x-56 gap-y-5 h-full shadow-2xl bg-[#f5f5f5] p-5 rounded-md">
+        <div className="">
+          <label>Área danificada</label>
+          <select className="">
+            <option value="">Selecione o caso problema</option>
+            {casosProblema.map((casoProblema) => (
+              <option value={casoProblema}>{casoProblema}</option>
+            ))}
+          </select>
         </div>
+        <div>
+          <label>Lesão na folha</label>
+          <select className="">
+            <option value="">Selecione o caso problema</option>
+            {casosProblema.map((casoProblema) => (
+              <option value={casoProblema}>{casoProblema}</option>
+            ))}
+          </select>
+        </div>
+        <div>
+          <label>Histórico da colheita</label>
+          <select className="">
+            <option value="">Selecione o caso problema</option>
+            {casosProblema.map((casoProblema) => (
+              <option value={casoProblema}>{casoProblema}</option>
+            ))}
+          </select>
+        </div>
+        <div>
+          <label>Data da colheita</label>
+          <select className="">
+            <option value="">Selecione o caso problema</option>
+            {casosProblema.map((casoProblema) => (
+              <option value={casoProblema}>{casoProblema}</option>
+            ))}
+          </select>
+        </div>
+        <div className="">
+          <label>Decaimento externo</label>
+          <select className="">
+            <option value="">Selecione o caso problema</option>
+            {casosProblema.map((casoProblema) => (
+              <option value={casoProblema}>{casoProblema}</option>
+            ))}
+          </select>
+        </div>
+        <div>
+          <label>Manchas na fruta</label>
+          <select className="">
+            <option value="">Selecione o caso problema</option>
+            {casosProblema.map((casoProblema) => (
+              <option value={casoProblema}>{casoProblema}</option>
+            ))}
+          </select>
+        </div>
+        <div>
+          <label>Taxa de germinação</label>
+          <select className="">
+            <option value="">Selecione o caso problema</option>
+            {casosProblema.map((casoProblema) => (
+              <option value={casoProblema}>{casoProblema}</option>
+            ))}
+          </select>
+        </div>
+        <div>
+          <label>Hall</label>
+          <select className="">
+            <option value="">Selecione o caso problema</option>
+            {casosProblema.map((casoProblema) => (
+              <option value={casoProblema}>{casoProblema}</option>
+            ))}
+          </select>
+        </div>
+        <div className="">
+          <label>Descoloramento</label>
+          <select className="">
+            <option value="">Selecione o caso problema</option>
+            {casosProblema.map((casoProblema) => (
+              <option value={casoProblema}>{casoProblema}</option>
+            ))}
+          </select>
+        </div>
+        <div>
+          <label>Folhagem</label>
+          <select className="">
+            <option value="">Selecione o caso problema</option>
+            {casosProblema.map((casoProblema) => (
+              <option value={casoProblema}>{casoProblema}</option>
+            ))}
+          </select>
+        </div>
+        <div>
+          <label>Manchas auréolas nas folhagem </label>
+          <select className="">
+            <option value="">Selecione o caso problema</option>
+            {casosProblema.map((casoProblema) => (
+              <option value={casoProblema}>{casoProblema}</option>
+            ))}
+          </select>
+        </div>
+        <div>
+          <label>Tamanho das manchas</label>
+          <select className="">
+            <option value="">Selecione o caso problema</option>
+            {casosProblema.map((casoProblema) => (
+              <option value={casoProblema}>{casoProblema}</option>
+            ))}
+          </select>
+        </div>
+        <div className="">
+          <label>Margem das manchas</label>
+          <select className="">
+            <option value="">Selecione o caso problema</option>
+            {casosProblema.map((casoProblema) => (
+              <option value={casoProblema}>{casoProblema}</option>
+            ))}
+          </select>
+        </div>
+        <div>
+          <label>Folhas</label>
+          <select className="">
+            <option value="">Selecione o caso problema</option>
+            {casosProblema.map((casoProblema) => (
+              <option value={casoProblema}>{casoProblema}</option>
+            ))}
+          </select>
+        </div>
+        <div>
+          <label>Alojamento</label>
+          <select className="">
+            <option value="">Selecione o caso problema</option>
+            {casosProblema.map((casoProblema) => (
+              <option value={casoProblema}>{casoProblema}</option>
+            ))}
+          </select>
+        </div>
+        <div>
+          <label>Crescimento de mofo</label>
+          <select className="">
+            <option value="">Selecione o caso problema</option>
+            {casosProblema.map((casoProblema) => (
+              <option value={casoProblema}>{casoProblema}</option>
+            ))}
+          </select>
+        </div>
+        <div className="">
+          <label>Crescimento da planta</label>
+          <select className="">
+            <option value="">Selecione o caso problema</option>
+            {casosProblema.map((casoProblema) => (
+              <option value={casoProblema}>{casoProblema}</option>
+            ))}
+          </select>
+        </div>
+        <div>
+          <label>Micélio</label>
+          <select className="">
+            <option value="">Selecione o caso problema</option>
+            {casosProblema.map((casoProblema) => (
+              <option value={casoProblema}>{casoProblema}</option>
+            ))}
+          </select>
+        </div>
+        <div>
+          <label>Suporte para planta</label>
+          <select className="">
+            <option value="">Selecione o caso problema</option>
+            {casosProblema.map((casoProblema) => (
+              <option value={casoProblema}>{casoProblema}</option>
+            ))}
+          </select>
+        </div>
+        <div>
+          <label>Precipitação</label>
+          <select className="">
+            <option value="">Selecione o caso problema</option>
+            {casosProblema.map((casoProblema) => (
+              <option value={casoProblema}>{casoProblema}</option>
+            ))}
+          </select>
+        </div>
+        <div className="">
+          <label>Raizes</label>
+          <select className="">
+            <option value="">Selecione o caso problema</option>
+            {casosProblema.map((casoProblema) => (
+              <option value={casoProblema}>{casoProblema}</option>
+            ))}
+          </select>
+        </div>
+        <div>
+          <label>Escleródios</label>
+          <select className="">
+            <option value="">Selecione o caso problema</option>
+            {casosProblema.map((casoProblema) => (
+              <option value={casoProblema}>{casoProblema}</option>
+            ))}
+          </select>
+        </div>
+        <div>
+          <label>Sementes</label>
+          <select className="">
+            <option value="">Selecione o caso problema</option>
+            {casosProblema.map((casoProblema) => (
+              <option value={casoProblema}>{casoProblema}</option>
+            ))}
+          </select>
+        </div>
+        <div>
+          <label>Descoloração das sementes</label>
+          <select className="">
+            <option value="">Selecione o caso problema</option>
+            {casosProblema.map((casoProblema) => (
+              <option value={casoProblema}>{casoProblema}</option>
+            ))}
+          </select>
+        </div>
+        <div>
+          <label>Tamanho das sementes</label>
+          <select className="">
+            <option value="">Selecione o caso problema</option>
+            {casosProblema.map((casoProblema) => (
+              <option value={casoProblema}>{casoProblema}</option>
+            ))}
+          </select>
+        </div>
+        <div>
+          <label>Aplicação nas sementes</label>
+          <select className="">
+            <option value="">Selecione o caso problema</option>
+            {casosProblema.map((casoProblema) => (
+              <option value={casoProblema}>{casoProblema}</option>
+            ))}
+          </select>
+        </div>
+        <div>
+          <label>Severidade</label>
+          <select className="">
+            <option value="">Selecione o caso problema</option>
+            {casosProblema.map((casoProblema) => (
+              <option value={casoProblema}>{casoProblema}</option>
+            ))}
+          </select>
+        </div>
+        <div>
+          <label>Grao de murchamento</label>
+          <select className="">
+            <option value="">Selecione o caso problema</option>
+            {casosProblema.map((casoProblema) => (
+              <option value={casoProblema}>{casoProblema}</option>
+            ))}
+          </select>
+        </div>
+        <div>
+          <label>Tronco</label>
+          <select className="">
+            <option value="">Selecione o caso problema</option>
+            {casosProblema.map((casoProblema) => (
+              <option value={casoProblema}>{casoProblema}</option>
+            ))}
+          </select>
+        </div>
+        <div>
+          <label>Doença no tronco</label>
+          <select className="">
+            <option value="">Selecione o caso problema</option>
+            {casosProblema.map((casoProblema) => (
+              <option value={casoProblema}>{casoProblema}</option>
+            ))}
+          </select>
+        </div>
+        <div>
+          <label>Temperatura</label>
+          <select className="">
+            <option value="">Selecione o caso problema</option>
+            {casosProblema.map((casoProblema) => (
+              <option value={casoProblema}>{casoProblema}</option>
+            ))}
+          </select>
+        </div>
+      </form>
+      <div className="flex w-24 mt-4">
+        <button type="submit" className="bg-gray-300 duration-500">Enviar</button>
       </div>
     </section>
   )
