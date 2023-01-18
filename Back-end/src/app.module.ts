a42dfada47400c205913f1c44b5e4d0e19b534dd
Felipe Henrique Camargos Ferreira <felipeferreira.sp.dev@gmail.com>
2023-01-18 17:43:30 -0300

    feat: route post cases and database complete

--- Back-end/src/app.module.ts
+++ Back-end/src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
+import { PrismaService } from './services/prisma/prisma.service';
+import { CasesModule } from './modules/cases/cases.module';
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [CasesModule],
+  controllers: [],
+  providers: [PrismaService],
+  exports: [PrismaService],
 })
 export class AppModule {}
