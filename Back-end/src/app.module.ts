ef1c76352661af8ded10018ddd105cd1efe9bab3
Felipe Henrique Camargos Ferreira <felipeferreira.sp.dev@gmail.com>
2023-01-21 17:35:05 -0300

    feat: implementação rotas get area damaged

--- Back-end/src/app.module.ts
+++ Back-end/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from './services/prisma/prisma.service';
 import { CasesModule } from './modules/cases/cases.module';
+import { AreaDamagedModule } from './modules/area_damaged/area_damaged.module';
 @Module({
-  imports: [CasesModule],
+  imports: [CasesModule, AreaDamagedModule],
   controllers: [],
   providers: [PrismaService],
   exports: [PrismaService],
