2b0d663f62fb5645833f940eb7d9c570190a1153
Felipe Henrique Camargos Ferreira <felipeferreira.sp.dev@gmail.com>
2023-01-22 22:01:59 -0300

    feat: implementação rota get geral para todas as tabelas exceto casos

--- Back-end/src/app.module.ts
+++ Back-end/src/app.module.ts
@@ -5,6 +5,7 @@ import { AreaDamagedModule } from './modules/area_damaged/area_damaged.module';
 import { CankerLesionModule } from './modules/canker_lesion/canker_lesion.module';
 import { CropHistModule } from './modules/crop_hist/crop_hist.module';
 import { DateModule } from './modules/date/date.module';
+import { AllModule } from './modules/all/all.module';
 @Module({
   imports: [
     CasesModule,
@@ -12,6 +13,7 @@ import { DateModule } from './modules/date/date.module';
     CankerLesionModule,
     CropHistModule,
     DateModule,
+    AllModule,
   ],
   controllers: [],
   providers: [PrismaService],
