6137044367daad219be6dc8259b124716b2481b4
GeovaneRigonato <geovane.rigonato.gil@gmail.com>
2023-01-25 21:41:37 -0300

    :lipstick: feat adicionando a rota /last

--- Back-end/src/modules/cases/cases.controller.ts
+++ Back-end/src/modules/cases/cases.controller.ts
@@ -4,26 +4,25 @@ import { CasesService } from './cases.service';
 
 @Controller('cases')
 export class CasesController {
-    constructor(private readonly casesService: CasesService) { }
+  constructor(private readonly casesService: CasesService) {}
 
-    @Post()
-    async createCase(@Body() data: Prisma.CasesCreateInput) {
-        return this.casesService.createCase(data);
-    }
+  @Post()
+  async createCase(@Body() data: Prisma.CasesCreateInput) {
+    return this.casesService.createCase(data);
+  }
 
-    @Get()
-    async findAll() {
-        return this.casesService.findAll();
-    }
+  @Get()
+  async findAll() {
+    return this.casesService.findAll();
+  }
 
-    @Get('/log')
-    async qualquerCoisa() {
-        return "qualquer coisa";
-    }
+  @Get('/log')
+  async qualquerCoisa() {
+    return 'qualquer coisa';
+  }
 
-    //@Get que retorna o ultimo caso criado
-    @Get('/last')
-    async lastCase() {
-        return this.casesService.lastCase();
-    }
+  @Get('/last')
+  async lastCase() {
+    return this.casesService.lastCase();
+  }
 }
