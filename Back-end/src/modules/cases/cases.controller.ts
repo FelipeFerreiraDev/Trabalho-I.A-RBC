f7265f507783dd4d2d25b06f831d65fcc3adcd6e
GeovaneRigonato <geovane.rigonato.gil@gmail.com>
2023-01-29 22:14:09 -0300

    :bug: fix arrumando rotas no backend

--- Back-end/src/modules/cases/cases.controller.ts
+++ Back-end/src/modules/cases/cases.controller.ts
@@ -4,31 +4,35 @@ import { CasesService } from './cases.service';
 
 @Controller('cases')
 export class CasesController {
-    constructor(private readonly casesService: CasesService) { }
+  constructor(private readonly casesService: CasesService) {}
 
-    @Post()
-    async createCase(@Body() data: Prisma.CasesCreateInput) {
-        return this.casesService.createCase(data);
-    }
+  @Post()
+  async createCase(@Body() data: Prisma.CasesCreateInput) {
+    return this.casesService.createCase(data);
+  }
 
-    @Get()
-    async findAll() {
-        return this.casesService.findAll();
-    }
+  @Get()
+  async findAll() {
+    return this.casesService.findAll();
+  }
 
-    @Get('/log')
-    async qualquerCoisa() {
-        return 'qualquer coisa';
-    }
+  @Get('/log')
+  async qualquerCoisa() {
+    return 'qualquer coisa';
+  }
 
-    @Get('/last')
-    async lastCase() {
-        return this.casesService.lastCase();
-    }
+  @Get('/last')
+  async lastCase() {
+    return this.casesService.lastCase();
+  }
 
-    @Get('/compare')
-    async compare() {
-        return this.casesService.compare();
-    }
+  @Get('/compare')
+  async compare() {
+    return this.casesService.compare();
+  }
 
+  @Get(':id')
+  async findOne(@Body() id: number) {
+    return this.casesService.findOne(id);
+  }
 }
