9cee31275703a30aaed000225b4ed57caed49c2f
Felipe Henrique Camargos Ferreira <felipeferreira.sp.dev@gmail.com>
2023-01-31 10:47:04 -0300

    feat: codigo sql

--- Back-end/src/modules/cases/cases.service.ts
+++ Back-end/src/modules/cases/cases.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { Prisma } from '@prisma/client';
+import { Sql, sqltag } from '@prisma/client/runtime';
 import { PrismaService } from 'src/services/prisma/prisma.service';
 
 @Injectable()
@@ -13,7 +14,45 @@ export class CasesService {
   }
 
   async findAll() {
-    const data = await this.prisma.cases.findMany();
+    const data = await this.prisma.cases.findMany({
+      include: {
+        area: true,
+        canker: true,
+        crop: true,
+        date_new: true,
+        external: true,
+        fruiting: true,
+        fruitpod: true,
+        fruits: true,
+        germination_new: true,
+        hail_new: true,
+        intdiscolor: true,
+        leafmalf: true,
+        leafmild: true,
+        leafshread: true,
+        leafspotshalo: true,
+        leafspotsmarg: true,
+        leafspotssize: true,
+        leaves_new: true,
+        lodging_new: true,
+        mold: true,
+        mycelium_new: true,
+        plant: true,
+        plantstand: true,
+        precip_new: true,
+        roots_new: true,
+        sclerotia_new: true,
+        seed_new: true,
+        seeddiscolor: true,
+        seedsize: true,
+        seedtmt: true,
+        severity_new: true,
+        shriveling_new: true,
+        stem_new: true,
+        stemcankers: true,
+        temp_new: true,
+      },
+    });
     return data;
   }
 
@@ -26,66 +65,68 @@ export class CasesService {
     return data;
   }
 
-  async compare() {
-    let contador = 0;
-    const data = await this.prisma.cases.findFirst({
-      orderBy: {
-        case: 'desc',
-      },
-    });
-
-    const allCases = await this.prisma.cases.findMany();
-
-    allCases.map((item) => {
-      if (
-        item.area_damaged === data.area_damaged &&
-        item.canker_lesion === data.area_damaged &&
-        item.crop_hist === data.crop_hist &&
-        item.date === data.date &&
-        item.external_decay === data.external_decay &&
-        item.fruiting_bodies === data.fruiting_bodies &&
-        item.fruit_pods === data.fruit_pods &&
-        item.fruits_spots === data.fruits_spots &&
-        item.germination === data.germination &&
-        item.hail === data.hail &&
-        item.int_discolor === data.int_discolor &&
-        item.leaf_malf === data.leaf_malf &&
-        item.leaf_mild === data.leaf_mild &&
-        item.leaf_shread === data.leaf_shread &&
-        item.leafspots_halo === data.leafspots_halo &&
-        item.leafspots_marg === data.leafspots_marg &&
-        item.leafspots_size === data.leafspots_size &&
-        item.leaves === data.leaves &&
-        item.lodging === data.lodging &&
-        item.mold_growth === data.mold_growth &&
-        item.mycelium === data.mycelium &&
-        item.plant_growth === data.plant_growth &&
-        item.plant_stand === data.plant_stand &&
-        item.precip === data.precip &&
-        item.roots === data.roots &&
-        item.sclerotia === data.sclerotia &&
-        item.seed === data.seed &&
-        item.seed_discolor === data.seed_discolor &&
-        item.seed_size === data.seed_size &&
-        item.seed_tmt === data.seed_tmt &&
-        item.severity === data.severity &&
-        item.shriveling === data.shriveling &&
-        item.stem === data.stem &&
-        item.stem_cankers === data.stem_cankers &&
-        item.temp === data.temp
-      ) {
-        contador++;
-      }
-    });
-    return contador;
-  }
-
   async findOne(id: number) {
-    const data = await this.prisma.cases.findUnique({
+    const data = await this.prisma.cases.findFirst({
       where: {
         case: id,
       },
+      include: {
+        area: true,
+        canker: true,
+        crop: true,
+        date_new: true,
+        external: true,
+        fruiting: true,
+        fruitpod: true,
+        fruits: true,
+        germination_new: true,
+        hail_new: true,
+        intdiscolor: true,
+        leafmalf: true,
+        leafmild: true,
+        leafshread: true,
+        leafspotshalo: true,
+        leafspotsmarg: true,
+        leafspotssize: true,
+        leaves_new: true,
+        lodging_new: true,
+        mold: true,
+        mycelium_new: true,
+        plant: true,
+        plantstand: true,
+        precip_new: true,
+        roots_new: true,
+        sclerotia_new: true,
+        seed_new: true,
+        seeddiscolor: true,
+        seedsize: true,
+        seedtmt: true,
+        severity_new: true,
+        shriveling_new: true,
+        stem_new: true,
+        stemcankers: true,
+        temp_new: true,
+      },
     });
     return data;
+  }
+
+  async calc(campo: string, table: string) {
+    // -- SQLite
+    // -- SELECT *,
+    // -- 1-(2-Area_damaged.peso)/(4-1)
+    // -- FROM CASES
+    // -- LEFT JOIN Area_damaged ON CASES.area_damaged = Area_damaged.area_damaged;
+    // -- SELECT MAX(Area_damaged.peso) FROM Area_damaged;
+    // -- SELECT MIN(Area_damaged.peso) FROM Area_damaged;
+    // y = valor que o usuário inseriu
+    // z = valor máximo
+    // x = valor mínimo
+    // SELECT *,
+    // 1-(y-Area_damaged.peso)/(z-x) AS area_damaged_sl,
+    // 1-(1-Canker_lesion.peso)/(3-0) AS canker_lesion_sl
+    // FROM CASES
+    // LEFT JOIN Area_damaged ON CASES.area_damaged = Area_damaged.area_damaged
+    // LEFT JOIN Canker_lesion ON CASES.canker_lesion = Canker_lesion.canker_lesion;
   }
 }
